b538c32a1d3e2cd18b5b2c33692fbc464ae93755
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-05 13:02:48 +0600

    banner done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './component/Banner/Banner';
 import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
 
     </div>
   );
